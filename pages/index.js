96c5dd80421a5405abcc60e68291be6beca7f16d
Mike Carbone <mfcbone2@gmail.com>
2020-01-16 16:55:06 -0500

    :rocket: First push

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from '../utils/headLinks';
+import Meta from '../utils/meta'
+
+export default () => (
+    <>
+    <Meta
+        title=""
+        description=""
+        img=""
+    />
+    <Head />
+    <h1>Hello</h1>
+
+    </>
+)
---
+++ utils/headLinks.js
@@ -0,0 +1,12 @@
+import '../styles/styles.scss'
+import Head from 'next/head';
+
+const NewHead = () => (
+    <>
+        <Head>
+            <link href="https://fonts.googleapis.com/css?family=Bebas+Neue|Noto+Sans+HK:100,300,400,700&display=swap" rel="stylesheet" />
+        </Head>
+    </>
+);
+
+export default NewHead;
